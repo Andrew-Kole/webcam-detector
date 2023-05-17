b6ec896e5a9a397a76ac2c771394caa37ae17c92
pakitowka <whitesaint12@gmail.com>
2023-05-17 13:00:40 +0200

    capture changes

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import cv2
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     time.sleep(1)
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-    cv2.imshow("My video", gray_frame_gau)
+
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    cv2.imshow("My video", delta_frame)
+
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
