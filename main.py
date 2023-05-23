60c1534933102f27a255df59ed3ab4fe243a09db
pakitowka <whitesaint12@gmail.com>
2023-05-23 02:02:23 +0200

    cosmetic

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+
 while True:
     status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
-
     if first_frame is None:
         first_frame = gray_frame_gau
-
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
     thresh_frame = cv2.threshold(delta_frame, 40, 255, cv2.THRESH_BINARY)[1]
@@ -30,7 +29,7 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
 
